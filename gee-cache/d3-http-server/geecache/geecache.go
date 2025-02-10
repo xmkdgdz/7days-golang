1e1110ad9ec3d50a0c1db91613fdba212691045b
xmkdgdz <xmkdgdz@foxmail.com>
2025-02-10 11:55:21 +0800

    Finish: gee-cache d1, d2, d3

---
+++ gee-cache/d3-http-server/geecache/geecache.go
@@ -0,0 +1,101 @@
+// 负责与外部交互，控制缓存存储和获取的主流程
+/*
+                            是
+接收 key --> 检查是否被缓存 -----> 返回缓存值 ⑴
+                |  否                         是
+                |-----> 是否应当从远程节点获取 -----> 与远程节点交互 --> 返回缓存值 ⑵
+                            |  否
+                            |-----> 调用`回调函数`，获取值并添加到缓存 --> 返回缓存值 ⑶ (回调函数由用户自定义，配置数据源，如文件、数据库等)
+
+*/
+
+package geecache
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+// 回调函数
+// A Getter loads data for a key.
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+// A GetterFunc implements Getter with a function.
+type GetterFunc func(key string) ([]byte, error)
+
+// Get implements Getter interface function
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+// Group 一个缓存的命名空间
+type Group struct {
+	name      string // 唯一
+	getter    Getter // 缓存未命中时的回调函数
+	mainCache cache
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+// NewGroup create a new instance of Group
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("getter is required")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+// GetGroup 返回对应 group 或 nil
+func GetGroup(name string) *Group {
+	mu.RLock() // 只读锁
+	g := groups[name]
+	mu.RUnlock()
+	return g
+}
+
+// Get value for a key from cache
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is empty")
+	}
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[GeeCache] hit")
+		return v, nil
+	}
+
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	return g.getLocally(key)
+}
+
+// 从本机调用用户的回调函数（单机）
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+// populateCache 将源数据添加到缓存 mainCache 中
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
